214357ad300f0be276ce8f3dd9c4eb2944044601
Andy James <jameandy@oregonstate.edu>
2022-08-05 16:10:03 -0700

    remove test file

--- database/db-connector.js
+++ database/db-connector.js
@@ -4,10 +4,10 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : '',
-    user            : '',
-    password        : '',
-    database        : ''
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : 'cs340_jameandy',
+    password        : '8855',
+    database        : 'cs340_jameandy'
 })
 
 // Export it for use in our application
